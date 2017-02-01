8a7eeafc404252d5857ace77ac604994c74746b1
Denis Tataurov <sineedus@mail.ru>
2017-02-01 19:58:19 +0300

    Encode URL fragment

--- lib/arc/definition/versioning.ex
+++ lib/arc/definition/versioning.ex
@@ -7,6 +7,10 @@ defmodule Arc.Definition.Versioning do
   end
 
   def resolve_file_name(definition, version, {file, scope}) do
+    resolve_file_name(definition, version, {file, scope}, Application.get_env(:arc, :secret))
+  end
+
+  defp resolve_file_name(definition, version, {file, scope}, nil) do
     name = definition.filename(version, {file, scope})
     conversion = definition.transform(version, {file, scope})
 
@@ -14,6 +18,11 @@ defmodule Arc.Definition.Versioning do
       {_, _, ext} -> "#{name}.#{ext}"
        _          -> "#{name}#{Path.extname(file.file_name)}"
     end
+  end
+  defp resolve_file_name(definition, version, {file, scope}, secret) do
+    file_name = resolve_file_name(definition, version, {file, scope}, nil)
+    token     = :crypto.hmac(:sha, secret, file_name) |> Base.encode16 |> String.downcase
+    token <> file_name
   end
 
   defmacro __before_compile__(_env) do
